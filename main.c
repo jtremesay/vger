25977754ac2e566541a3d8867642a9f129b28ac6
Solene Rapenne <solene@perso.pw>
2020-12-02 13:29:21 +0100

    Fix the License name, it's BSD2 and not MIT

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2020 Solène Rapenne <solene@openbsd.org>
  * 
- * MIT LICENSE
+ * BSD 2-clause License
  * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
